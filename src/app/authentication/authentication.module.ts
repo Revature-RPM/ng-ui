dce0f6986ca035ba49c13c4b3db8b1f3cf5d0e95
Sahil Makhijani <svmakhijani94@gmail.com>
2018-11-30 15:08:39 -0500

    Created auth components

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LogoutComponent,
+    RegistrationComponent
+  ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
