4f8a6b55874eb88578f26a9c8adde7d03bf90dd5
Shawn Bickel <sbickel201@gmail.com>
2018-11-30 10:30:39 -0500

    added home page component to project module

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProjectRoutingModule } from './project-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomePageComponent],
   imports: [
     CommonModule,
     ProjectRoutingModule
