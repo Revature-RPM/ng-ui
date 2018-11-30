214e2e73062af6097c9f66150edb22bc0ec424bf
Sahil Makhijani <svmakhijani94@gmail.com>
2018-11-30 16:06:22 -0500

    Created user interface for data modeling

---
+++ src/app/core/models/User.ts
@@ -0,0 +1,9 @@
+export interface User{
+    id?: number;
+    firstname?: string;
+    lastname?: string;
+    username?: string;
+    password?: string;
+    email?: string;
+    userRole?: string;
+ }
