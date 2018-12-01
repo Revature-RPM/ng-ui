49aac1a7de8b8d9393cfc81cc441bcebf43f8a61
Shawn Bickel <sbickel201@gmail.com>
2018-12-01 15:21:35 -0500

    enabled Administrator view in the NavBar of the user home

--- src/app/project/navbar/navbar.component.ts
+++ src/app/project/navbar/navbar.component.ts
@@ -9,12 +9,13 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class NavbarComponent implements OnInit {
 
-  isAdministrator: Boolean;
+  isAdministrator: Boolean = true;
   /**
 	 * the constructor is called when an instance of the class is created
 	 * 
 	 * @param iconRegistry:  a service to register icons so they can be used with the mat-icon component from Angular Materials
-   * @param sanitizer: prevents cross-site scripting attacks by filtering values to be used in the DOM
+   * @param sanitizer: prevents cross-site scripting attacks by filtering values to be used in the DOM; in this case it is bypassing
+   *                         Angular's default security to use this image asset
 	 * @author Shawn Bickel (1810-Oct08-Java-USF)
 	 */
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { 
