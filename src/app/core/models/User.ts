7fb7f441d46f318dc8c2caba5d3612d3948ba85a
Yuki Mano <ysm@umail.ucsb.edu>
2018-11-30 19:04:02 -0500

    delete user model

--- src/app/core/models/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id?: number;
-    firstname?: string;
-    lastname?: string;
-    username?: string;
-    password?: string;
-    email?: string;
-    userRole?: string;
- }
