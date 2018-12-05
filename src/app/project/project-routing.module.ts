57060e04586f82b50b221e8874199bb0fe147438
Sahil Makhijani <svmakhijani94@gmail.com>
2018-12-05 14:28:16 -0500

    Update project-routing.module.ts

--- src/app/project/project-routing.module.ts
+++ src/app/project/project-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectSubmissionComponent } from './project-submission/project-submission.component';
-import { ZipComponentComponent } from '../zip-file-explorer/zip-component/zip-component.component'
+import { ZipComponent } from '../zip-file-explorer/zip/zip.component'
 
 const routes: Routes = [
   { path: '', component: HomePageComponent, pathMatch: 'full' },
