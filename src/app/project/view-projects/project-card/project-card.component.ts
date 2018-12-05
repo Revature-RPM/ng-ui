d2bc3f13ee1fc375be25d840e233c6a3b91b7149
Sahil Makhijani <svmakhijani94@gmail.com>
2018-12-05 13:42:36 -0500

    Update project-card.component.ts

--- src/app/project/view-projects/project-card/project-card.component.ts
+++ src/app/project/view-projects/project-card/project-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input  } from '@angular/core';
 import { Project } from '../../../core/models/Project';
 import { User } from '../../../core/models/User';
-import { ProjectServiceService } from 'src/app/core/services/project-service.service';
+import { ProjectService } from 'src/app/core/services/project.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,29 +16,17 @@ export class ProjectCardComponent implements OnInit {
   approved: Boolean = false;  
   loggedInUser: User;
   currentimage: string;
-  constructor(private projectservice: ProjectServiceService, private router: Router) {
-      
-   }
+  
+  constructor(private projectservice: ProjectService, private router: Router) {
+  }
 
   ngOnInit() {
-    // this.loggedInUser = JSON.parse(localStorage.getItem('user'));
-    // if(this.loggedInUser.userRole === 'admin'){
-    //   this.admin = true;
-    // }else {
-    //   this.admin = false;
-    // }
-
     console.log(this.project.techStack[0])
   }
+  
   changeImage(){
-    // console.log("change image")
-    // if(this.project){
-    //   if(this.currentimage==this.project.teamImage)
-    //     this.currentimage = this.project.screenshots[0];
-    //   else
-    //     this.currentimage=this.project.teamImage;
-    // }
   }
+  
   openCodeBase(){
     this.projectservice.setCurrentProject(this.project);
     this.router.navigate(['projects/codebase']);
