e06baac0d703f8bbfdcb7ac76dbcdae79145ae73
Sahil Makhijani <svmakhijani94@gmail.com>
2018-12-06 16:30:48 -0500

    Removed unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatJumbotronModule } from '@angular-material-extensions/jumbotron';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatJumbotronModule } from '@angular-material-extensions/jumbotron';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ProjectModule } from './project/project.module';
