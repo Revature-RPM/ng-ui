f343b07ebbf5688de5d5e851e7b27364c4ef04ba
Sahil Makhijani <svmakhijani94@gmail.com>
2018-12-06 10:59:14 -0500

    Added consistency to imports

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 
 import { User } from 'src/app/core/models/User';
-import { UserService } from '../../core/services/user.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +11,13 @@ import { UserService } from '../../core/services/user.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  sessionUser = false; // localStorage.getItem('user');
+  sessionUser = localStorage.getItem('user');
   user: User = {};
 
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
-    if (this.sessionUser) {
+    if (this.sessionUser !== null) {
       this.router.navigate(['']);
     }
   }
