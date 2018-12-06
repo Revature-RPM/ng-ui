8a69c632d09d8cef55ebed9c3da15f9d325d4da4
Sahil Makhijani <svmakhijani94@gmail.com>
2018-12-06 09:27:58 -0500

    I undid my own breaking mistakes

--- src/app/zip-file-explorer/zip/zip.component.ts
+++ src/app/zip-file-explorer/zip/zip.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import * as JSZip from 'jszip';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-zip-component',
@@ -28,7 +27,7 @@ export class ZipComponent implements OnInit {
   RenderFile: RenderFile[] = [];
   SelectedFile: RenderFile;
   OpenFile: RenderFile[] = [];
-  filepath: string = '';
+  filepath = '';
   /*Constructur: Injects Http Client into the component for use of resource request
   *@param HttpClient standard angular dependency to fire http request.
   *
@@ -36,12 +35,12 @@ export class ZipComponent implements OnInit {
   constructor(private http: HttpClient, private location: Location) { }
 
   ngOnInit() {
-    let testfile = new RenderFile();
+    const testfile = new RenderFile();
     testfile.fileName = 'HELP';
     testfile.fileContent = 'HELLO';
     this.SelectedFile = testfile;
   }
-  
+
   goBack() {
     this.location.back();
   }
@@ -52,10 +51,10 @@ export class ZipComponent implements OnInit {
   *
   */
   sendRequest() {
-    //reponse type is arraybuffer so the get request knows this is a oclet-array-stream request
-    this.http.get('http://localhost:8080/spring-mvc/files',{ observe: 'response', responseType: 'blob'})
+    // reponse type is arraybuffer so the get request knows this is a oclet-array-stream request
+    this.http.get('http://localhost:8080/spring-mvc/files', { observe: 'response', responseType: 'blob'})
     .subscribe(blob => {
-      //after the array is retrieve. open the data with JSZip
+      // after the array is retrieve. open the data with JSZip
       console.log('got (ui8Arra)');
       console.log(blob);
       console.log(blob.body);
@@ -63,12 +62,12 @@ export class ZipComponent implements OnInit {
       console.log(blob.headers.get('content-disposition'));
       const datafilename = this.getFileNameFromHttpResponse(blob.headers.get('content-disposition'));
       console.log(datafilename);
-      this.openData(blob.body,datafilename);
+      this.openData(blob.body, datafilename);
     });
   }
-  
+
   getFileNameFromHttpResponse(contentDispositionHeader) {
-    var result = contentDispositionHeader.split(';')[1].trim().split('=')[1];
+    const result = contentDispositionHeader.split(';')[1].trim().split('=')[1];
     return result.replace(/"/g, '');
   }
   /*
@@ -77,48 +76,49 @@ export class ZipComponent implements OnInit {
   * @param data. ui8array blob object that "is" a valid zip file.
   * @author Andrew Mitchem (1810-Oct08-Java-USF)
   */
-   openData(data , datafilename?) {
-     console.log('This is your data file: ' + datafilename);
-     this.RenderFile= [];
-     const testfile = new  RenderFile();
-     testfile.fileName = 'HELP';
-     testfile.fileContent = 'HELLO';
-     this.SelectedFile = testfile;
-     this.OpenFile = [];
-     console.log("This is your data: " + data);
-     let dataname ='';
-     if (data.name)
-       dataname = data.name.substring(0,data.name.lastIndexOf("."));
-     else
-       dataname = datafilename.substring(0,datafilename.lastIndexOf("."));
-     console.log(dataname);
-     const zip = new JSZip(); 
-     //new instance of JSZip. note this object lifecycle needs to be undone after rendering
-     //as such it not a class member but function member only for the scope of this function closure
-     zip.loadAsync(data)
-        .then(contents => {
-          console.log(contents);
-          //move to the sub folder inside the zip file: replace with pass paramater variables
-          const dirFolder = zip.folder(dataname);
+  openData(data , datafilename?) {
+    console.log('This is your data file: ' + datafilename);
+    this.RenderFile = [];
+    const testfile = new  RenderFile();
+    testfile.fileName = 'HELP';
+    testfile.fileContent = 'HELLO';
+    this.SelectedFile = testfile;
+    this.OpenFile = [];
+    console.log('This is your data: ' + data);
+    let dataname = '';
+    if (data.name) {
+      dataname = data.name.substring(0, data.name.lastIndexOf('.'));
+    } else {
+      dataname = datafilename.substring(0, datafilename.lastIndexOf('.'));
+    }
+    console.log(dataname);
+    const zip = new JSZip();
+    // new instance of JSZip. note this object lifecycle needs to be undone after rendering
+    // as such it not a class member but function member only for the scope of this function closure
+    zip.loadAsync(data)
+      .then(contents => {
+        console.log(contents);
+        // move to the sub folder inside the zip file: replace with pass paramater variables
+        let dirFolder = zip.folder(dataname);
+        console.log(dirFolder);
+        console.log(dirFolder.folder(/src\/main\/java/));
+        if (dirFolder.folder(/src\/main\/java/).length) {
+          console.log('Hi');
+          dirFolder =  dirFolder.folder('src/main/java');
           console.log(dirFolder);
-          console.log(dirFolder.folder(/src\/main\/java/));
-          if (dirFolder.folder(/src\/main\/java/).length) {
-            console.log('Hi');
-            dirFolder =  dirFolder.folder('src/main/java');
-            console.log(dirFolder);
-            this.filepath = dataname + '/src/main/java';
-          } else { 
-            console.log('Hello');
-            dirFolder =  dirFolder.folder('src/app');
-            console.log(dirFolder);
-            this.filepath = dataname + '/src/app';
-          }
-          const fileArray = dirFolder.file(/^.*/); //get the array of all files in this subdirectory 
-          for (let i = 0; i < fileArray.length; i++) {
-            const file = fileArray[i];
-            this.parseFiles(file);
-          }
-      });
+          this.filepath = dataname + '/src/main/java';
+        } else {
+          console.log('Hello');
+          dirFolder =  dirFolder.folder('src/app');
+          console.log(dirFolder);
+          this.filepath = dataname + '/src/app';
+        }
+        const fileArray = dirFolder.file(/^.*/); // get the array of all files in this subdirectory
+        for (let i = 0; i < fileArray.length; i++) {
+          const file = fileArray[i];
+          this.parseFiles(file);
+        }
+    });
   }
    /*
   * ZipComponent.parseFiles(file)
