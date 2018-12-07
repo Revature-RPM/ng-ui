effe625e571753aa5ca96d68a25080db6bccf4ec
Shawn Bickel <sbickel201@gmail.com>
2018-12-07 16:16:36 -0500

    ensuring that dat sent to the server is valid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, enableTracing: true })],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
