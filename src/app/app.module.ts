a948f1f7cce09ade57240646b5379009f0e73bb4
Sahil Makhijani <svmakhijani94@gmail.com>
2018-12-11 11:55:45 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { MatJumbotronModule } from '@angular-material-extensions/jumbotron';
+import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { NgMetaModule } from 'ngmeta';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,12 +11,10 @@ import { ProjectModule } from './project/project.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { environment } from 'src/environments/environment';
-import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor} from 'src/app/core/services/jwtInterceptor.interceptor'
+
 export function tokenGetter() {
-      console.log(window.localStorage.getItem('jwt'))
-      return window.localStorage.getItem('jwt')
+      return window.localStorage.getItem('jwt');
 }
 
 @NgModule({
