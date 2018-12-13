b55e4231604ff72efe05b33c6ebb1d2404fec25f
mitchem <andrewmitchem70@yahoo.com>
2018-12-13 14:46:45 -0500

    comments in user service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -42,6 +42,11 @@ ${error.error}`
     this.jwtauthtoken = null;
     this.user = null;
   }
+  /*
+  * retrievers the current user from user service. if null for whatever reason. checks local storage for valid
+  * user and jwt
+  *@Author Andrew Mitchem
+  */
   getUser() {
     if (this.user) {
       return this.user;
@@ -71,14 +76,18 @@ ${error.error}`
       }), catchError(this.handleError));
   }
 
-    // only use environment.url for the base url and concat any restful endpoints
+   /*
+   *Register a new user
+   */
   register(user: User): Observable<User> {
     user.role = 'user';
     return this.http.post<User>(environment.url+ '/auth/users/', user, httpOptions)
       .pipe(catchError(this.handleError));
   }
 
-    // only use environment.url for the base url and concat any restful endpoints
+  /*
+  * Updates the user profile
+  */
   updateProfile(user: User): Observable<User> {
     return this.http.put<User>(environment.url + '/auth/users/', user, httpOptions)
       .pipe(catchError(this.handleError));
