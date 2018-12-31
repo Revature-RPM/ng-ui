fb7166b9bf8658e336b6892250330eebbce3fa64
rrwilliams <rrwilliams1600@gmail.com>
2018-12-31 09:59:08 -0500

    First unit tests in Jasmine

--- src/app/project/view-users-projects/view-users-projects.component.spec.ts
+++ src/app/project/view-users-projects/view-users-projects.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { SharedModule } from '../../shared/shared.module';
+import { AppModule} from '../../app.module';
 import { ViewUsersProjectsComponent } from './view-users-projects.component';
+
+
+
+/**
+ * This test suite serves to check the proper creation of the ViewProjects
+ * component as well the as well as the functionality
+ * of the various methods within it.
+ * @author Ryan Beevers | Shawn Bickel | Sahil Makhijani | Andrew Mitchem | Yuki Mano | Jeffly Luctamar | (1810-Oct08-Java-USF)
+ * @author Ryan Williams | Michael Grammens | (1810-Oct22-Java-USF)
+ */
 
 describe('ViewUsersProjectsComponent', () => {
   let component: ViewUsersProjectsComponent;
@@ -8,7 +22,8 @@ describe('ViewUsersProjectsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewUsersProjectsComponent ]
+      declarations: [ ],
+      imports: [ SharedModule, RouterTestingModule, BrowserAnimationsModule, AppModule]
     })
     .compileComponents();
   }));
--- src/app/project/view-users/view-users.component.spec.ts
+++ src/app/project/view-users/view-users.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { SharedModule } from '../../shared/shared.module';
+import { AppModule} from '../../app.module';
 import { ViewUsersComponent } from './view-users.component';
+
+
+
+/**
+ * This test suite serves to check the proper creation of the ViewProjects
+ * component as well the as well as the functionality
+ * of the various methods within it.
+ * @author Ryan Beevers | Shawn Bickel | Sahil Makhijani | Andrew Mitchem | Yuki Mano | Jeffly Luctamar | (1810-Oct08-Java-USF)
+ * @author Ryan Williams | Michael Grammens | (1810-Oct22-Java-USF)
+ */
 
 describe('ViewUsersComponent', () => {
   let component: ViewUsersComponent;
@@ -8,7 +22,8 @@ describe('ViewUsersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewUsersComponent ]
+      declarations: [ ],
+      imports: [ SharedModule, RouterTestingModule, BrowserAnimationsModule, AppModule]
     })
     .compileComponents();
   }));
