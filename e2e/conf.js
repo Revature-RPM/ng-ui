7b85c7fdca38f16522583e11fb803bf28590acec
ojamal02 <ojamal02@gmail.com>
2019-04-05 15:05:15 -0400

    working path to tests, currently failing due to AWS purge

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,5 +1,5 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js', './protractor-spec/admin-logic-spec.js']
 }
