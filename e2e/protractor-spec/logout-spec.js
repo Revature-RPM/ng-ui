0f33c20d636eeb32fe5c46e8612f94b88d82fce6
AbeSchroeder <abe.schroeder75@gmail.com>
2019-04-15 09:55:41 -0400

    three working tests

--- e2e/protractor-spec/logout-spec.js
+++ e2e/protractor-spec/logout-spec.js
@@ -5,10 +5,11 @@
  * @author Abe Schroeder, Omar Jamal, Zach Marazita, Thanh Nguyen, Mitchell Elbus
  */
 let url = 'http://tn-rpm-test.s3-website-us-east-1.amazonaws.com/';
-beforeAll(function(){
+beforeEach(function(){
     browser.get(`${url}`);
     element(by.id('mat-input-0')).sendKeys('test-user');
     element(by.id('mat-input-1')).sendKeys('test');
+    // browser.pause('4444');
     browser.actions().sendKeys(protractor.Key.ENTER).perform();
     browser.waitForAngular();
     
@@ -17,6 +18,7 @@ describe('Testing logout', function(){
     
     it('Should return to the login page', function(){
         browser.get(`${url}home`);
+        // browser.pause('4444');
         let myButton = element.all(by.tagName('button')).get(3);
         myButton.click();
         browser.waitForAngular(
