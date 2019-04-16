8f2c4748467047dcc4978ba6eeeb0a84683002e3
Alex Johnson <alexjohnson4564@gmail.com>
2019-04-15 21:46:36 -0400

    Added a test to home-page component

--- src/app/project/home-page/home-page.component.spec.ts
+++ src/app/project/home-page/home-page.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { SharedModule } from '../../shared/shared.module';
 import { AppModule} from '../../app.module';
 import { HomePageComponent } from './home-page.component';
+import { UserService } from 'src/app/core/services/user.service';
 
 /**
  * This test suite serves to check the proper creation of the HomePage
@@ -16,6 +18,8 @@ import { HomePageComponent } from './home-page.component';
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let router: Router;
+  let service: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,9 +32,28 @@ describe('HomePageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(UserService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /**
+   * testing that when the home-page component is rendered, if the user is null
+   * then the user should be navigated back to login
+   * 
+   * @author Alex Johnson (190107-Java-Spark-USF)
+   */
+  it('should navigate to login if the user is null', () => {
+
+    router = TestBed.get(Router);
+    localStorage.clear();
+    localStorage.setItem('user', null);
+    let navigateSpy = spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/auth/login']);
   });
 });
