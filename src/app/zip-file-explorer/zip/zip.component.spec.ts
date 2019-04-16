cc7a0c2eaf0de0105cd456ea3cffa282a57bbecf
Alex Johnson <alexjohnson4564@gmail.com>
2019-04-16 16:02:37 -0400

    Committing changes to zip component

--- src/app/zip-file-explorer/zip/zip.component.spec.ts
+++ src/app/zip-file-explorer/zip/zip.component.spec.ts
@@ -8,6 +8,7 @@ import { AppModule} from '../../app.module';
 import { ZipComponent } from './zip.component';
 import { ZipFileExplorerModule } from '../zip-file-explorer.module';
 import { ProjectService } from 'src/app/core/services/project.service';
+import { Observable } from 'rxjs';
 
 describe('ZipComponent', () => {
   let component: ZipComponent;
@@ -15,6 +16,11 @@ describe('ZipComponent', () => {
   let router: Router;
   let projectService:ProjectService;
   let renderFile: any;
+  class RenderFile {
+    fileName: String;
+    fileContent: String;
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ],
@@ -35,33 +41,6 @@ describe('ZipComponent', () => {
   });
   
   /**
-   * This test should display error messages
-   * @Author Gabriel Zapata and Edward Bechtold (190107-Java-Spark-USF)
-   */
-  xit('should throw an error', () => {
-    
-    fixture.detectChanges();
-    let message = 'test';
-    
-    component.errorFile(message);
-    
-    expect(message).toBeTruthy();
-  })
-
-  /**
-   * This tests the safeTitle method to ensure links are being properly created
-   */
-  xit('should return a substring of link', () => {
-    
-    fixture.detectChanges();
-    let link = 'test';
-    
-    component.safeTitle(link);
-
-    expect(component.safeTitle).toBe('test/');
-  });
-
-  /**
    * testing that when the zip component is rendered, if the user is null
    * then the user should be navigated back to login
    * 
@@ -81,68 +60,155 @@ describe('ZipComponent', () => {
   });
   
   /**
-   * Test will verify goBack location
+   * This test should display error messages
+   * @author Gabriel Zapata | Edward Bechtold (190107-Java-Spark-USF)
    */
-  xit('should test goBack and location.back to be called',() =>{
+  xit('should call ErrorFile an error', () => {
     
     fixture.detectChanges();
-    component.goBack();
-  })
-  /**
-   * Test test error file
-   * not working  
-   */
-  xit('should test errorFile ',()=>{
+    let message = 'test';
     
-    fixture.detectChanges();
-    let message = 'test'
     component.errorFile(message);
     
+    expect(message).toBeTruthy();
   })
+
   /**
-   * Test test error file 
-   * not working
+   * This tests the safeTitle method to ensure links are being properly created
+   * 
+   * @author Gabriel Zapata | Edward Bechtold (190107-Java-Spark-USF)
    */
-  xit('should test safeFile ',()=>{
+  xit('should return a substring of link', () => {
     
     fixture.detectChanges();
-    let message = 'test'
-    component.safeTitle(message);
-  })
+    let link = 'test';
+    
+    component.safeTitle(link);
+
+    expect(link).toBe('test');
+  });
+
+  /**
+   * Test will verify ngOnInit field SelectedFile to be truthy
+   * 
+   * @author Gabriel Zapata (190107-Java-Spark-USF)
+   */
+
+  xit('should verify to fields if the user is NOT null', () => {
+    
+    let testFile : RenderFile
+    testFile ={
+      fileName: 'testFileName',
+      fileContent: 'testFileContent'
+    }
+
+
+    spyOn(component,'defaultFile').and.returnValue(testFile);
+    component.ngOnInit();
+
+    expect(component.SelectedFile).toBeTruthy();
+  });
+
+
    /**
    * Test test openRenderFile 
-   * not working
+   * 
+   * @author Gabriel Zapata (190107-Java-Spark-USF)
+   * 
    */
-  xit('should test openRenderFile ',()=>{
-    
-    fixture.detectChanges();
-    renderFile = 'test';
-    component.OpenFile.push(renderFile);
-    component.safeTitle(renderFile);
-    expect(component.SelectedFile).toBe(renderFile);
-  })
-    /**
-   * Test test openRenderFile 
-   * not working
-   */
-  xit('should test openRenderFile ',()=>{
-    
-    fixture.detectChanges();
-    renderFile = 'test';
-    component.OpenFile.push(renderFile);
-    component.safeTitle(renderFile);
-    expect(component.SelectedFile).toBe(renderFile);
-  })
-     /**
-   * Test test openRenderFile 
-   * not working
-   */
-  xit('should test getFileNameFromHpResponse',()=>{
-    
-    fixture.detectChanges();
-    let message = 'test'
-    component.getFileNameFromHttpResponse(message);
+  it('should test openRenderFile and verify that testFile is being added to OpenFile ',()=>{
+    let testFile : RenderFile
+    testFile ={
+      fileName: 'testFileName',
+      fileContent: 'testFileContent'
+    }
+    component.OpenFile = [];
+
+    component.openRenderFile(testFile);
+
+    expect(component.SelectedFile).toBe(testFile);
+    expect(component.OpenFile).toContain(testFile);
+
   })
 
+   /**
+   * Test test closeRenderFile 
+   * 
+   * @author Gabriel Zapata (190107-Java-Spark-USF)
+   * 
+   */
 
+  it('should test closeRenderFile if OpenFile array removes testFile but still contain testFile2  ',()=>{
+    let testFile : RenderFile
+    testFile ={
+      fileName: 'testFileName',
+      fileContent: 'testFileContent'
+    }
+    let testFile2 : RenderFile
+    testFile ={
+      fileName: 'testFileName',
+      fileContent: 'testFileContent'
+    }
+
+    component.OpenFile = [testFile,testFile2];
+    spyOn(component,'defaultFile').and.returnValue(testFile2);
+
+    component.closeRenderFile(testFile);
+
+    expect(component.OpenFile).toContain(testFile2);
+    expect(component.SelectedFile).toBe(testFile2);
+
+  })
+
+   /**
+   * Test getFileNameFromHttpResponse is called  
+   * 
+   * @author Gabriel Zapata (190107-Java-Spark-USF)
+   * 
+   */
+
+  it('should test getFileNameFromHttpResponse with testContentDispositionHeader',()=>{
+    let testContentDispositionHeader = ['test=1; test=2; test=3;'].toString();
+    component.getFileNameFromHttpResponse(testContentDispositionHeader);
+
+
+  })
+  /**
+   * Test openData
+   * 
+   * @author Gabriel Zapata (190107-Java-Spark-USF)
+   */
+
+  it('should verify RenderFile, Select, OpenFile, with data.name is truthy should be truthy',() =>{
+    let data={
+      name: 'testName'
+    }
+    let datafilename;
+
+    component.openData(data,datafilename);
+
+    expect(component.OpenFile).toBeTruthy();
+   
+
+  })
+
+   /**
+   * Test openData
+   * 
+   * @author Gabriel Zapata (190107-Java-Spark-USF)
+   */
+
+  it('should verify RenderFile, Select, OpenFile, with data.name is falsy OpenFile should be truthy',() =>{
+    let data={
+      name: ''
+    }
+    let datafilename = 'test'
+
+
+    component.openData(data,datafilename);
+
+    expect(component.OpenFile).toBeTruthy();
+  })
+
+   
 });
