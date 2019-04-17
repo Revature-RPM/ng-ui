d9094abcc8330ddaebc8e41560c0c3e59fb4ee93
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-04-16 20:59:46 -0400

    approve project as admin

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -6,12 +6,13 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './protractor-spec/admin-login-spec.js', 
+    './protractor-spec/admin-approve-project-spec.js',
+    './protractor-spec/admin-login-spec.js',
     './protractor-spec/logout-spec.js',
-    './protractor-spec/updateProf-spec.js',
+    './protractor-spec/updateprof-spec.js',
     './protractor-spec/submit-project-route-spec.js',
     './protractor-spec/register-user-spec.js',
-    './protractor-spec/submitProject-spec.js'
+    './protractor-spec/submitproject-spec.js'
   ],
   capabilities: {
     'browserName': 'chrome'
