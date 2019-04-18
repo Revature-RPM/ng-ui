bb80653c7b40c60c23023385c3622afa051c4ad2
AkshayaPk <ashpeekay23@gmail.com>
2019-04-18 10:15:27 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   // url: 'http://rpm-elb-996808586.us-east-1.elb.amazonaws.com/rpm-zuul'
   // url: 'http://rpm-alb-1871573764.eu-west-2.elb.amazonaws.com/zuul'
   // url: 'http://localhost:8762'
-  url: 'rpmapi.revaturelabs.com/zuul'
+  url: 'http://rpm-264761131.us-east-1.elb.amazonaws.com/zuul'
 };
