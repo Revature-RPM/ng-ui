bb8598a53461bb7fd9ab4d86a39931ca170058ee
Kelly Young <kelly.regina.young@gmail.com>
2019-07-10 08:28:05 -0400

    Working on Submit a Project styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,10 @@ import { ProjectSubmissionPageComponent } from './components/pages/project-submi
 import { ProjectEditComponent } from './components/pages/project-edit/project-edit.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
-  {path: 'auth/login', component: LoginRegisterPageComponent},
+  {path: '', component: ProjectSubmissionPageComponent},
+  // {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  // {path: 'auth/login', component: LoginRegisterPageComponent},
+  {path: 'auth/login', component: ProjectSubmissionPageComponent},
   {path: 'projects', component: ProjectGridPageComponent},
   {path: 'projects/1', component: ProjectGridPageComponent},
   {path: 'submitform', component: ProjectSubmissionPageComponent},
