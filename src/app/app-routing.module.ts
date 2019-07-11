a12dea4eaa32c6aebb88da19004521c3462e9479
Kelly Young <kelly.regina.young@gmail.com>
2019-07-10 20:42:53 -0400

    Made an ellipsis pipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,8 @@ import { ProjectEditComponent } from './components/pages/project-edit/project-ed
 import { ProfileComponent } from './components/pages/user-management/profile/profile.component';
 
 const routes: Routes = [
-  // {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
-  // {path: 'auth/login', component: LoginRegisterPageComponent},
-  {path: 'auth/login', component: ProjectGridPageComponent},
+  {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  {path: 'auth/login', component: LoginRegisterPageComponent},
   {path: 'projects', component: ProjectGridPageComponent},
   {path: 'projects/1', component: ProjectGridPageComponent},
   {path: 'submitform', component: ProjectSubmissionPageComponent},
