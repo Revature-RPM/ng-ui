db43601809a0a65a9bd8efef0c1dccde597e7243
KerrJ <justin.kerr98@gmail.com>
2019-07-12 09:26:36 -0400

    documentation for nav-menu component.ts

--- src/app/components/HUD/nav-menu/nav-menu.component.ts
+++ src/app/components/HUD/nav-menu/nav-menu.component.ts
@@ -18,6 +18,14 @@ export class NavMenuComponent implements OnInit {
   constructor(private projectService: ProjectService, private userService: UserService, private router: Router) {
   }
 
+   /**
+   * On component instantiation:
+   * Subscribes to the user service to keep track of the user who is logged in.
+   * The user is used to keep track of the nav-menu display rather than the jwt because
+   * the nav-menu needs to be aware of session changes at every page and an observable is the most
+   * reliable way to constantly check for these changes. You cannot subscribe to an item in local storage.
+   * @author Justin Kerr (190422-USF)
+   */
   ngOnInit() {
 
     this.userService.user.asObservable().subscribe(
@@ -32,11 +40,21 @@ export class NavMenuComponent implements OnInit {
     );
   }
 
+   /**
+   * Function that:
+   * Calls the user service logout function and re-routes to the login page.
+   */
   logout() {
     this.userService.logout();
     this.router.navigate(['auth/login']);
   }
 
+  /**
+   * Function that:
+   * Sets the type of project view to local storage (type can be set to 'view' or 'all').
+   * Both types are routed to the project-grid-page component, but a /1 is denoted for user projects.
+   * The 1 was a test variable but can be changed to more appropriately represent the user Id.
+   */
   getProjects(type) {
         localStorage.removeItem('viewprojects');
         localStorage.setItem('viewprojects', type);
@@ -44,10 +62,18 @@ export class NavMenuComponent implements OnInit {
         else this.router.navigate(['projects'])
   }
 
+  /**
+   * Function that:
+   * Navigates to the project-submission-page component.
+   */
   goToSubmit() {
     this.router.navigate(['submitform']);
   }
 
+  /**
+   * Function that:
+   * Navigates to the profile component.
+   */
   goToProfile() {
     this.router.navigate(['profile']);
   }
