b4191b81d48584a09cee3ce0a7189c79ec118d63
Wezley Singleton <wezley.singleton@gmail.com>
2019-07-17 12:49:47 -0400

    Update environment.prod.ts
    Removed commented out endpoints which were confusing and incorrect.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // url: 'http://rpm-elb-996808586.us-east-1.elb.amazonaws.com/rpm-zuul'
   url: 'http://rpm-264761131.us-east-1.elb.amazonaws.com/zuul'
-  // url: 'http://localhost:8762'
 };
