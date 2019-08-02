6bd67803053843769c182d5b9ca4f9f5fadf150b
devwithdonald <hend.donald@gmail.com>
2019-08-02 15:31:18 -0400

    Added unit test for project-welcome-page component

--- src/app/components/pages/project/project-welcome-page/project-welcome-page.component.spec.ts
+++ src/app/components/pages/project/project-welcome-page/project-welcome-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectWelcomePageComponent } from './project-welcome-page.component';
 
 describe('ProjectWelcomePageComponent', () => {
@@ -8,9 +7,9 @@ describe('ProjectWelcomePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectWelcomePageComponent ]
+      declarations: [ProjectWelcomePageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +20,15 @@ describe('ProjectWelcomePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display title text correctly', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Revature Project Manager');
+  });
+
+  it('should display description text correctly', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain('The Revature Project Management (RPM) system is a web application that can be used to record metadata information regarding completed full-stack projects. Authenticated users can submit project information, which is then approved by an administrator. Using repository links provided during project submission, project codebases will be pulled and made available for viewing. This application will provide administrators the ability to deploy the application onto dynamically allocated cloud resources so that it can be evaluated in the state it was in during its original presentation.');
   });
 });
