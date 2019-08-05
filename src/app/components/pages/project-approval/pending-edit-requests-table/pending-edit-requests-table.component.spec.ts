ca708460d2f7452a95a542d9e5c4e48cf8ab4a43
devwithdonald <hend.donald@gmail.com>
2019-08-05 16:26:17 -0400

    setup test for pending edit requests table

--- src/app/components/pages/project-approval/pending-edit-requests-table/pending-edit-requests-table.component.spec.ts
+++ src/app/components/pages/project-approval/pending-edit-requests-table/pending-edit-requests-table.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PendingEditRequestsTableComponent } from './pending-edit-requests-table.component';
+import { Project } from 'src/app/models/Project';
 
 describe('PendingEditRequestsTableComponent', () => {
   let component: PendingEditRequestsTableComponent;
   let fixture: ComponentFixture<PendingEditRequestsTableComponent>;
+  let row: Project;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,5 +24,15 @@ describe('PendingEditRequestsTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit on click', () => {
+    spyOn(component.swapProject, 'emit');
+    const nativeElement = fixture.nativeElement;
+    const matRow = nativeElement.querySelector('mat-row');
+    matRow.dispatchElement(new Event('click'));
+    fixture.detectChanges();
+
+    expect(component.swapProject.emit).toHaveBeenCalledWith(this.row);
   });
 });
