280bb36711207637a06199c70eaad602d9d9e503
glozy <glozy94@gmail.com>
2019-08-06 18:05:15 -0400

    Completed user story - As an admin, I can change the roles of other existing users

--- src/app/components/pages/user-management/admin-change-roles/admin-change-roles.component.ts
+++ src/app/components/pages/user-management/admin-change-roles/admin-change-roles.component.ts
@@ -3,7 +3,6 @@ import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { User } from '../../../../models/User';
 import { Router } from '@angular/router';
 import { UserService } from '../../../../services/user.service';
-import { SnackbarService } from 'src/app/services/snackbar.service';
 
 let users: User[] = [
 ];
@@ -21,17 +20,18 @@ export class AdminChangeRolesComponent implements OnInit {
     'admin',
     'user'
   ];
-  allUsersArray: User[];
   displayedColumns: string[] = ['ID#', 'Name', 'Role', 'Username', 'Email'];
   displayedColumnsData: string[] = ['id', 'firstName', 'lastName', 'email', 'username', 'role'];
   dataSource = new MatTableDataSource(users);
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private router: Router, private userService: UserService, private snackbar: SnackbarService) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
-    if (!localStorage.getItem('jwt')) this.router.navigate(['/auth/login']);
+    if (!localStorage.getItem('jwt')) {
+      this.router.navigate(['/auth/login']);
+    }
     this.userService.getAllUsers().subscribe(
       response => {
         users = response;
@@ -40,29 +40,34 @@ export class AdminChangeRolesComponent implements OnInit {
       });
   }
 
+  /**
+   * This function keeps track of all the changes made on the table. 
+   * @author Zak Noori (1905-Java-USF)
+   * @author Glory Umeasalugo (1905-Java-USF)
+   * @author Aisha Hodge (1905-Java-USF)
+   * @author Toyin Fadiran (1905-Java-USF)
+   */
   userChanged(user) {
     if (!updatedRoles.includes(user)) {
       updatedRoles.push(user);
     }
   }
 
-  logData(row) {
-   }
-
-  updateChanges() {
-    console.log('beginning of postchanges');
+  /**
+   * This function saves all changes made on the table and updates a role. 
+   * @author Zak Noori (1905-Java-USF)
+   * @author Glory Umeasalugo (1905-Java-USF)
+   * @author Aisha Hodge (1905-Java-USF)
+   * @author Toyin Fadiran (1905-Java-USF)
+   */
+  updateToAdmin() {
     updatedRoles.forEach(user => {
       this.userService.updateUserToAdmin(user).subscribe(
         response => {
           console.log(response);
         }
       );
-      console.log('end of changes');
     });
-  }
-
-  test() {
-    console.log(updatedRoles);
   }
 
   applyFilter(filterValue: string) {
