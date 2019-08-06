5c55cb5c7a6535974de91650904c6cceba1fa864
devwithdonald <hend.donald@gmail.com>
2019-08-06 15:02:54 -0400

    added unit tests to selected-project-viewer component

--- src/app/components/pages/project-approval/selected-project-viewer/selected-project-viewer.component.spec.ts
+++ src/app/components/pages/project-approval/selected-project-viewer/selected-project-viewer.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SelectedProjectViewerComponent } from './selected-project-viewer.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('SelectedProjectViewerComponent', () => {
   let component: SelectedProjectViewerComponent;
@@ -8,18 +11,48 @@ describe('SelectedProjectViewerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SelectedProjectViewerComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NoopAnimationsModule
+      ],
+      declarations: [SelectedProjectViewerComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectedProjectViewerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call approveProject Method', async(() => {
+    spyOn(component, 'approveProject');
+
+    const button = fixture.debugElement.nativeElement.querySelector('#approve-project-btn');
+    button.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.approveProject).toHaveBeenCalled();
+    });
+
+  }));
+
+  it('should call denyProject Method', async(() => {
+    spyOn(component, 'denyProject');
+
+    const button = fixture.debugElement.nativeElement.querySelector('#deny-project-btn');
+    button.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.denyProject).toHaveBeenCalled();
+    });
+
+  }));
+
 });
