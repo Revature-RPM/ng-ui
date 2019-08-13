ca4c34609d56336b69a78e30230b1c7315a57777
Mike James <techiegenin@gmail.com>
2019-08-13 10:07:25 -0400

    Fixes Dev issue

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -73,3 +73,4 @@ export class ProjectService {
  submitEditRequest(project: Project): Observable<Project> {
    return this.httpClient.post(environment.url + `/project/edit`, project, HTTP_OPTIONS);
  }
+}
