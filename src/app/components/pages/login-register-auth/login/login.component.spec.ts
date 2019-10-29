4dc7b6e76519b17255d667acb0c3ef7d6a0cfeaf
John Werenski <jtwerenski@gmail.com>
2019-10-29 13:31:45 -0400

    Test for redirection when a user is already logged in.

--- src/app/components/pages/login-register-auth/login/login.component.spec.ts
+++ src/app/components/pages/login-register-auth/login/login.component.spec.ts
@@ -4,6 +4,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgMetaService } from 'ngmeta';
+import { Router } from '@angular/router';
 
 import { MatFormFieldModule, MatProgressSpinnerModule, MatIconModule, MatInputModule } from '@angular/material';
 
@@ -12,6 +13,8 @@ import {LoginComponent} from './login.component';
 fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let router: Router;
+  let routerSpy;
 
 
   beforeEach(async(() => {
@@ -30,7 +33,15 @@ fdescribe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.get(Router);
+    routerSpy = spyOn(router, 'navigate').and
+    .callFake( function() { return null; });
   });
+
+  afterEach(() => {
+    localStorage.removeItem('jwt');
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -44,4 +55,16 @@ fdescribe('LoginComponent', () => {
     expect(component.loggedIn).toBeFalsy();
     expect(component.logSuccess).toBeTruthy();
   })
+
+  it('redirects to projects if user is logged in already', () => {
+    localStorage.setItem('jwt', "fake");
+    component.ngOnInit();
+
+    expect(routerSpy).toHaveBeenCalledWith(['projects']);
+  })
+
+  it('Submitting the form calls login', () => {
+    
+  })
+
 });
