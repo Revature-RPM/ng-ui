0af9bcbbfdd5a8366c062fff786c1f22a9ad2af2
iagosergiop@gmail.com <iagosergiop@gmail.com>
2019-10-29 09:33:28 -0400

    Toolbar fixed

--- src/app/components/HUD/sidenav/sidenav.component.ts
+++ src/app/components/HUD/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -11,9 +11,20 @@ export class SidenavComponent implements OnInit {
   log(state) {
     console.log(state);
   }
-  constructor(private router: Router) { }
+
+  loggedIn:boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((e) => {
+      if(e instanceof NavigationEnd) {
+        this.loggedIn = e.url.includes("auth/login") ? false : true;
+        console.log("loggedIn", this.loggedIn);
+      }
+    });
+  }
 
   ngOnInit() {
+
   }
 
   routeToProfile() {
