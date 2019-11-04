47e2cda29a944de05611431c9d8cd8872993493c
John Werenski <jtwerenski@gmail.com>
2019-11-04 15:02:28 -0500

    Add declarations AboutComponent and ProjectListComponent to HomePageComponent spec as well as imports MatCardModule, MatToolTipModule, RouterTestingModule, and HttpClientTestingModule.

--- src/app/components/pages/home-page/home-page.component.spec.ts
+++ src/app/components/pages/home-page/home-page.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
+import { AboutComponent } from '../about/about.component';
+import { ProjectListComponent } from '../project/project-list/project-list.component';
+import { MatCardModule, MatTooltipModule} from '@angular/material';
+import {Router, RouterModule} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('HomePageComponent', () => {
+
+fdescribe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
+      declarations: [ HomePageComponent, AboutComponent, ProjectListComponent ],
+      imports: [MatCardModule, MatTooltipModule, RouterModule, RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   }));
