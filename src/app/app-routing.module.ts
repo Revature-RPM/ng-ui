c964226c51c1f3d997d45901dfb9294ca70a8e74
John Werenski <jtwerenski@gmail.com>
2019-11-07 14:46:12 -0500

    Remove welcome page component from app module and delete project-grid-page component and remove from app module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { AdminGuard } from './guards/admin.guard';
 // Components
 import {PageNotFoundComponent} from './components/pages/page-not-found/page-not-found.component';
 import {LoginRegisterPageComponent} from './components/pages/login-register-auth/login-register-page/login-register-page.component';
-import {ProjectGridPageComponent} from './components/pages/project/project-grid-page/project-grid-page.component';
 import { ProjectSubmissionPageComponent } from './components/pages/project-submission/project-submission-page/project-submission-page.component';
 import { ProjectEditComponent } from './components/pages/project-edit/project-edit.component';
 import { ProfileComponent } from './components/pages/user-management/profile/profile.component';
