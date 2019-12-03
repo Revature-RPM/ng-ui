c78206d965616b77f9da58b1fd3abb71f7a19354
CNTeske <cnt3pb@mst.edu>
2019-12-03 12:03:10 -0500

    Added notification navigator.

--- src/app/components/pages/notifications/notifications.component.ts
+++ src/app/components/pages/notifications/notifications.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { trigger, state, transition, animate, style } from '@angular/animations';
 import { User } from 'src/app/models/User';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -21,11 +21,13 @@ import { BehaviorSubject } from 'rxjs';
     ]),
   ],
 })
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent implements OnInit, OnChanges {
   title: string = "My Notifications";
   currentUser: User;
   userId: string;
   notificationList: Notification[];
+  pageNumber = 1;
+  pageMax = 7;
 
   constructor(private router: Router, private userService: UserService, private notificationService: NotificationsService,
     private filterService: ProjectFilterService, private route: ActivatedRoute, private http: HttpClient) { }
@@ -43,16 +45,47 @@ export class NotificationsComponent implements OnInit {
     if (this.router.url.includes('notification')) {
       this.userId = this.currentUser.id + "";
     }
-    this.getNotifications(this.currentUser, 2);
+    this.getNotifications(this.currentUser, this.pageNumber);
+  }
+
+  ngOnChanges(){
+    this.getNotifications(this.currentUser, this.pageNumber);
+    console.log("change detected")
   }
 
   getNotifications(userObject: User, n: number):Notification[] {
     this.notificationService.getNotificationPage(userObject.id, n).subscribe(notices => {
         this.notificationList = notices;
-      console.log(notices);
+      //this.pageMax = XXXXXXXXXXXXXXXXXXXXXX
     });
     return this.notificationList;
   };
+  firstPage(){
+    this.pageNumber = 1;
+    this.changedPage();
+  }
+  previousPage(){
+    if (this.pageNumber>1)
+      this.pageNumber--;
+      this.changedPage();  
+  }
+  nextPage(){
+    if (this.pageNumber<this.pageMax)
+      this.pageNumber++;
+    this.changedPage();
+  }
+  lastPage(){
+    this.pageNumber = this.pageMax;
+    this.changedPage();
+  }
+  changedPage(){
+    Math.floor(this.pageNumber);
+    if (this.pageNumber<1){
+      this.pageNumber = 1;}
+    if (this.pageNumber>this.pageMax){
+      this.pageNumber = this.pageMax;}
+    this.getNotifications(this.currentUser, this.pageNumber);
+  }
 }
 
 
