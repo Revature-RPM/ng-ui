c6652d82a4618576fc57f1a21676463d87028011
Martin <38227325+mcordero96@users.noreply.github.com>
2019-12-04 14:33:40 -0500

    all tests working and up to date, waiting on new notification tests

--- src/app/services/notifications.service.spec.ts
+++ src/app/services/notifications.service.spec.ts
@@ -1,9 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import { NotificationsService } from './notifications.service';
+import { HttpClient } from 'selenium-webdriver/http';
+import { AppModule } from '../app.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NotificationsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let injector: TestBed;
+  let httpMock: HttpClientTestingModule;
+  let service;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    declarations: [ ],
+      imports: [RouterTestingModule, HttpClientTestingModule, AppModule], //AuthenticationModule],
+    providers: [NotificationsService]
+    });
+    injector = getTestBed();
+    service = injector.get(NotificationsService);
+    httpMock = injector.get(HttpClientTestingModule);
+    localStorage.setItem('jwt', 'testJWT');
+  });
+
 
   it('should be created', () => {
     const service: NotificationsService = TestBed.get(NotificationsService);
